f61a4a4ddb800aef363fb360736cbd0b53433315
Matheus Tregnago <maugustotreg@gmail.com>
2026-04-12 17:31:04 -0300

    feat: test cuda job

--- flake.nix
+++ flake.nix
@@ -4,26 +4,52 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
         python-env = pkgs.python3.withPackages (p: [
           p.jupyter
           p.notebook
           p.pandas
           p.matplotlib
           p.numpy
+          p.seaborn
+          p.torch
+          p.transformers
+          p.vllm
         ]);
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = [ python-env ];
-        buildInputs = with pkgs; [
-          marp-cli
-          texliveFull
-          chromium
-        ];
-      };
-    }
-  );
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = [ python-env ];
+            buildInputs = with pkgs; [
+              marp-cli
+              texliveFull
+              chromium
+              basedpyright
+              python311Packages.python
+            ];
+          };
+          pcad = pkgs.mkShell {
+            packages = [ python-env ];
+            buildInputs = with pkgs; [
+              cudaPackages.nsight_compute
+              cudaPackages.nsight_systems
+              cudaPackages.cudatoolkit
+            ];
+          };
+        };
+      }
+    );
 }
