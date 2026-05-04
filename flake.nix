038736eb4947b9380b1e92fea4753551a8421323
RafaelSdeS <rafesilvadesouza@gmail.com>
2026-05-04 17:23:43 -0300

    add: new graphs to telemetry

--- flake.nix
+++ flake.nix
@@ -1,22 +1,20 @@
 {
   description = "Comp sys perf analysis";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+
       let
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
         };
+
         python-env = pkgs.python3.withPackages (p: [
           p.jupyter
           p.notebook
@@ -26,29 +24,35 @@
           p.seaborn
           p.torch
           p.transformers
-          p.vllm
+          p.accelerate
         ]);
-      in
-      {
+
+      in {
         devShells = {
+
           default = pkgs.mkShell {
-            packages = [ python-env ];
-            buildInputs = with pkgs; [
-              marp-cli
-              texliveFull
-              chromium
-              basedpyright
-              python311Packages.python
+            packages = [
+              python-env
+              pkgs.marp-cli
+              pkgs.texliveFull
+              pkgs.chromium
+              pkgs.basedpyright
             ];
           };
+
           pcad = pkgs.mkShell {
-            packages = [ python-env ];
-            buildInputs = with pkgs; [
-              cudaPackages.nsight_compute
-              cudaPackages.nsight_systems
-              cudaPackages.cudatoolkit
+            packages = [
+              python-env
+              pkgs.cudaPackages.cudatoolkit
+              pkgs.cudaPackages.nsight_compute
+              pkgs.cudaPackages.nsight_systems
             ];
+
+            shellHook = ''
+              export LD_LIBRARY_PATH=${pkgs.cudaPackages.cudatoolkit}/lib:$LD_LIBRARY_PATH
+            '';
           };
+
         };
       }
     );
